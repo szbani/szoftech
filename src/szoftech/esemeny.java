6e7d7a1db52f2b67a90a4516b44d1d5dc917a744
MondjukRoli <91498124+MondjukRoli@users.noreply.github.com>
2022-11-12 00:21:12 +0100

    Update esemeny.java

--- src/szoftech/esemeny.java
+++ src/szoftech/esemeny.java
@@ -8,7 +8,6 @@ import java.util.Vector;
 
 
 public class esemeny {
-    int id;
     private String név;
     String leírás;
     int kezdora, vegora;    // 8-22    
@@ -19,13 +18,11 @@ public class esemeny {
     // esemenyre jelentkezés user hozz
     
 
-    public esemeny(int id,String nev,String leiras,int év,int hónap,int nap,int kezdora,int vegora) {
-        this.id=id;
+    public esemeny(String nev,String leiras,int év,int hónap,int nap,int kezdora,int vegora) {
         this.név=nev;
         this.leírás=leiras;
         //calendar
-        this.nap= new GregorianCalendar(év,hónap,nap);
-        
+        this.nap= new GregorianCalendar(év,hónap,nap);        
         this.kezdora=kezdora;
         this.vegora=vegora;
         
@@ -41,9 +38,20 @@ public class esemeny {
         return vegora;
     }
 
+    public void kiir(){
+        System.out.println(this.név+" foglaló: "+this.getFoglaló()+"\n\t\t"+this.leírás+"\n\t\t"+this.nap.toString()+", kezdés: "+this.kezdora+"-"+this.vegora+" óráig");
+    }
     @Override
     public String toString() {
-        return "\t"+this.id+". "+this.név+" foglaló: "+this.getFoglaló()+"\n\t\t"+this.leírás+"\n\t\t"+this.nap.toString()+", kezdés: "+this.kezdora+"-"+this.vegora+" óráig";
+        String ret = this.név+","+this.leírás+","+this.kezdora+","+this.vegora+","+this.nap.toString()+",";
+        for (String string : resztvevők) {
+            ret+=","+string;
+        }
+        return ret;
+    }
+
+    void addUser(String us){
+        resztvevők.add(us);
     }
     void addUser(user us){
         resztvevők.add(us.getNev());
