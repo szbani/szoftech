62304b95d1ff99fee7371f9e04f700df8360651d
MondjukRoli <91498124+MondjukRoli@users.noreply.github.com>
2022-11-11 17:11:31 +0100

    Update user.java

--- user.java
+++ user.java
@@ -4,4 +4,27 @@ package szoftech;
 
 public class user {
     
+    private long id;
+    private final String user;
+    protected String pw;
+
+    public user(String user,String pw) {
+        this.user=user;
+        this.pw=pw;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public String getUser() {
+        return user;
+    }
+    
+
+
 }
