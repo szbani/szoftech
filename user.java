cdd995ec636e401782e77de2372f9edfebcbb422
MondjukRoli <91498124+MondjukRoli@users.noreply.github.com>
2022-11-11 17:41:51 +0100

    asd

--- user.java
+++ user.java
@@ -3,16 +3,31 @@ package szoftech;
 
 
 public class user {
-    
-    private long id;
-    private final String user;
+    private static long idCount;
+    private final long id;
+    private String user;
     protected String pw;
 
+    public user() {
+        idCount+=1;
+        id=idCount;
+        this.user="";
+    }
     public user(String user,String pw) {
+        idCount+=1;
+        id=idCount;
         this.user=user;
         this.pw=pw;
     }
 
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public void setUserName(String user) {
+        this.user = user;
+    }
+    
     public long getId() {
         return id;
     }
--- usercontainer.java
+++ usercontainer.java
@@ -1,18 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
+package szoftech;
+import java.util.*;
 
-/**
- *
- * @author Admin
- */
 public class usercontainer {
+    private Vector<user> usLst;
 
-    /**
-     * @param args the command line arguments
-     */
+    public usercontainer() {
+        this.usLst = new Vector<user>();
+        
+    }
+    
+    
     public static void main(String args[]) {
-        // TODO code application logic here
+        
     }
 }
