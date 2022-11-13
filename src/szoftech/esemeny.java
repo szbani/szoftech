1ecf7dbd71a3128fee178e481907d34575723630
MondjukRoli <91498124+MondjukRoli@users.noreply.github.com>
2022-11-13 18:28:46 +0100

    asd

--- src/szoftech/esemeny.java
+++ src/szoftech/esemeny.java
@@ -61,13 +61,13 @@ public class esemeny {
     }
     @Override
     public String toString() {
-        String ret = this.nev+"#"+this.leiras+"#"+this.nap+"#"+this.kezdora+"#"+this.vegora+"@"+this.ertekelesek;
+        String ret = this.nev+"#"+this.leiras+"#"+this.nap+"#"+this.kezdora+"#"+this.vegora;
         for (String string : resztvevok) {
-            ret+="#"+string;
+            ret+=","+string;
         }
-        
+        ret+="#";
         for (String string : ertekelesek) {
-            ret+="#"+string;
+            ret+=","+string;
         }
         return ret;
     }
